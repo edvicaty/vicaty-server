e94271dba7f4592743beb43896eaedea0c7f44c4
edvicaty <edvicaty@hotmail.com>
2020-09-28 15:51:57 -0500

    changes for deploy ready, first attempt

--- routes/user.js
+++ routes/user.js
@@ -11,16 +11,6 @@ const {
   createCreatedModel,
   editCreatedModel,
   deleteCreatedModel,
-  getAllElements,
-  getElement,
-  createElement,
-  deleteElement,
-  addSingle,
-  updateSingle,
-  deleteSingleData,
-  viewAllProjects,
-  getProject,
-  getCreatedModel,
 } = require("../controllers/user");
 const { isAuth, checkRole, catchErrors } = require("../middlewares");
 //projects CRUD
@@ -36,20 +26,5 @@ router.get("/model/:modelId", isAuth, viewCreatedModel);
 router.post("/model/:projectId", isAuth, createCreatedModel);
 router.put("/model/:modelId", isAuth, editCreatedModel);
 router.delete("/model/:modelId", isAuth, deleteCreatedModel);
-
-//Created Models elements (API endpoints to be delivered)
-router.post("/project", viewAllProjects);
-router.post("/project/:projectId", getProject);
-router.post("/createdModel/:modelId", getCreatedModel);
-router.post("/element/getAll/:modelId/", getAllElements);
-router.post("/element/getSingle/:modelId/:elementName", getElement);
-router.post("/element/create/:modelId", createElement);
-router.post("/element/delete/:modelId/:elementName", deleteElement);
-router.post("/element/addSingle/:modelId/:elementName", addSingle);
-router.put("/element/updateSingle/:modelId/:elementName/:dataId", updateSingle);
-router.post(
-  "/element/deleteSingle/:modelId/:elementName/:dataId",
-  deleteSingleData
-);
 
 module.exports = router;
